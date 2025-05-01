c7760e9f148524fffda07823dac1c3cbdf9dad81
Thomas <thomas.conquisti@gmail.com>
2025-05-01 14:49:24 -0300

    .

---
+++ stories/tsButton.stories.ts
@@ -0,0 +1,32 @@
+import { storiesOf } from '@storybook/html';
+import buildAndRegisterCustomElement from './utils/build-custom-element';
+import cButton from '../src/c/customButton/customButton';
+
+buildAndRegisterCustomElement('c-custom-button', cButton);
+
+storiesOf('CustomButton', module)
+	.add('custom base', () => `
+		<div class="slds-p-around_medium">
+			<c-custom-button label="Base Button"></c-custom-button>
+		</div>
+	`)
+	.add('custom neutral', () => `
+		<div class="slds-p-around_medium">
+			<c-custom-button variant="neutral" label="Neutral Button"></c-custom-button>
+		</div>
+	`)
+	.add('custom brand', () => `
+		<div class="slds-p-around_medium">
+			<c-custom-button variant="brand" label="Brand Button"></c-custom-button>
+		</div>
+	`)
+	.add('custom destructive', () => `
+		<div class="slds-p-around_medium">
+			<c-custom-button variant="destructive" label="Destructive Button"></c-custom-button>
+		</div>
+	`)
+	.add('custom success', () => `
+		<div class="slds-p-around_medium">
+			<c-custom-button variant="success" label="Success Button"></c-custom-button>
+		</div>
+	`);
--- stories/utils/build-custom-element.js
+++ stories/utils/build-custom-element.js
@@ -1,9 +1,8 @@
 import { buildCustomElementConstructor } from 'lwc';
 
 export default function buildAndRegisterCustomElement(elementName, elementClass) {
-    if (!customElements.get(elementName)) {
-        const customElement = buildCustomElementConstructor(elementClass);
-        customElements.define(elementName, customElement);
-    }
+	if (!customElements.get(elementName)) {
+		const customElement = buildCustomElementConstructor(elementClass);
+		customElements.define(elementName, customElement);
+	}
 }
-
