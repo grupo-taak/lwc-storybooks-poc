f0c330f0dd6406e1b64a3e4bf5d9726a18ff77b6
Thomas <thomas.conquisti@gmail.com>
2025-05-01 23:52:59 -0300

    Update customButton2.stories.js

--- stories/customButton2.stories.js
+++ stories/customButton2.stories.js
@@ -1,34 +1,52 @@
 import { storiesOf } from '@storybook/html';
-import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
+import { withKnobs, text, boolean, number, select } from '@storybook/addon-knobs';
 import buildAndRegisterCustomElement from './utils/build-custom-element';
 import cButton from 'c/customButton';
 
 buildAndRegisterCustomElement('c-custom-button', cButton);
 
-// export default {
-// 	title: 'Custom Buttom 2'
-// };
-
-// export const Base = () => `
-// 	<div class="slds-p-around_medium">
-// 		<c-custom-button label="Base Button"></c-custom-button>
-// 	</div>
-// `;
-
-// export const Brand  = () => `
-// 	<div class="slds-p-around_medium">
-// 		<c-custom-button variant="brand" label="Brand Button"></c-custom-button>
-// 	</div>
-// `;
-
-const label = 'Base Button 2'
-
 storiesOf('CustomButton2', module)
+	.addDecorator(withKnobs)
+	.add('button controls', () => {
+		const variantOptions = {
+			base: 'base',
+			neutral: 'neutral',
+			brand: 'brand',
+			outline: 'outline',
+			success: 'success',
+			destructive: 'destructive'
+		};
+
+		const label = text('label', 'Hello World');
+		const variant = select('variant', variantOptions, 'base');
+		const disabled = boolean('disabled', false);
+		console.log('disabled =>', disabled);
+
+		return `
+			<div class="slds-p-around_medium">
+				<c-custom-button
+					label='${label}'
+					variant='${variant}'
+					${disabled ? 'disabled' : ''}
+				>
+				</c-custom-button>
+
+				<pre><code>&lt;c-custom-button label='${label}' variant='${variant}' ${disabled ? 'disabled' : ''} &gt; &lt;/c-custom-button&gt;</code></pre>
+
+				&lt;c-custom-button
+					label='${label}'
+					variant='${variant}'
+					${disabled ? 'disabled' : ''}
+				&gt;
+				&lt;/c-custom-button&gt;
+			</div>
+		`;
+	})
 	.add('custom base', () => `
 		<div class="slds-p-around_medium">
-			<c-custom-button label='${label}'></c-custom-button>
+			<c-custom-button label="Base Button"></c-custom-button>
 		</div>
-	`).addParameters({label: 'test'})
+	`)
 	.add('custom neutral', () => `
 		<div class="slds-p-around_medium">
 			<c-custom-button variant="neutral" label="Neutral Button"></c-custom-button>
