68d755df26b53b2118c1ea3c3a69448d8f2984d8
Thomas <thomas.conquisti@gmail.com>
2025-05-01 21:03:13 -0300

    Revert "Update customButton2.stories.js"
    This reverts commit 2f3f4527903763594a08fdecea6dc282a33ffd2c.

--- stories/customButton2.stories.js
+++ stories/customButton2.stories.js
@@ -1,15 +1,34 @@
 import { storiesOf } from '@storybook/html';
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
 import buildAndRegisterCustomElement from './utils/build-custom-element';
 import cButton from 'c/customButton';
 
 buildAndRegisterCustomElement('c-custom-button', cButton);
 
+// export default {
+// 	title: 'Custom Buttom 2'
+// };
+
+// export const Base = () => `
+// 	<div class="slds-p-around_medium">
+// 		<c-custom-button label="Base Button"></c-custom-button>
+// 	</div>
+// `;
+
+// export const Brand  = () => `
+// 	<div class="slds-p-around_medium">
+// 		<c-custom-button variant="brand" label="Brand Button"></c-custom-button>
+// 	</div>
+// `;
+
+const label = 'Base Button 2'
+
 storiesOf('CustomButton2', module)
 	.add('custom base', () => `
 		<div class="slds-p-around_medium">
-			<c-custom-button label="Base Button"></c-custom-button>
+			<c-custom-button label='${label}'></c-custom-button>
 		</div>
-	`)
+	`).addParameters({label: 'test'})
 	.add('custom neutral', () => `
 		<div class="slds-p-around_medium">
 			<c-custom-button variant="neutral" label="Neutral Button"></c-custom-button>
